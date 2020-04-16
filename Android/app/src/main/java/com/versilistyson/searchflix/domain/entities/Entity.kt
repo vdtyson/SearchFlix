fb16ddab53d7ecf294a78d4fd5868fbc225b2ee1
vdtyson <versilistyson@gmail.com>
2020-04-15 22:40:53 -0400

    Add secrets and update gitignore

--- Android/app/src/main/java/com/versilistyson/searchflix/domain/entities/Entity.kt
+++ Android/app/src/main/java/com/versilistyson/searchflix/domain/entities/Entity.kt
@@ -2,7 +2,7 @@ package com.versilistyson.searchflix.domain.entities
 
 sealed class Entity
 
-sealed class Media(val name: String, val summary: String, val imagePath: String) : Entity() {
+sealed class Media(val name: String = "", val summary: String = "", val imagePath: String = "") : Entity() {
     data class Movie(
         val title: String = "",
         val releaseDate: String = "",
@@ -15,7 +15,11 @@ sealed class Media(val name: String, val summary: String, val imagePath: String)
     data class Show(
         val title: String = "",
         val overview: String = "",
-        val posterPath: String
+        val posterPath: String = ""
     ) : Media(title, overview, posterPath)
+}
+
+sealed class Category(val title: String, val mediaList: MutableList<Media>) {
+    data class PopularMovies(val movies: MutableList<Media.Movie>) : Category("Popular Movies", movies.toMutableList())
 }
 
