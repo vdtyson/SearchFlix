0d2ab22e48d2085d96376a9a683e1b2803d528f6
vdtyson <versilistyson@gmail.com>
2020-04-22 23:23:39 -0400

    Edit MovieDetails Fragment XML

--- Android/app/src/main/java/com/versilistyson/searchflix/MediaDetailsFragment.kt
+++ Android/app/src/main/java/com/versilistyson/searchflix/MediaDetailsFragment.kt
@@ -5,18 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.navArgs
+import com.squareup.picasso.Picasso
+import com.versilistyson.searchflix.data.util.NetworkConstants
+import com.versilistyson.searchflix.databinding.FragmentMediaDetailsBinding
+import com.versilistyson.searchflix.presentation.common.activity.DataBindingScreen
 
 /**
  * A simple [Fragment] subclass.
  */
-class MediaDetailsFragment : Fragment() {
+class MediaDetailsFragment : Fragment(), DataBindingScreen<FragmentMediaDetailsBinding> {
+
+    override lateinit var binding: FragmentMediaDetailsBinding
+    private val args: MediaDetailsFragmentArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_media_details, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_media_details, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Picasso.get().load(NetworkConstants.TMDB_DEFAULT_IMAGE_BASE_URL + args.media.posterPath).into(binding.ivPoster)
+        Picasso.get().load(NetworkConstants.TMDB_DEFAULT_IMAGE_BASE_URL + args.media.backdropPath).into(binding.ivBackDrop)
+    }
 }
