21bcc7b0600b54c1238ea90985812e571f493551
Versilis Tyson <versilistyson@gmail.com>
2020-05-20 20:55:36 -0500

    Add ShowApi

--- Android/app/src/main/java/com/versilistyson/searchflix/data/remote/api/ShowApi.kt
+++ Android/app/src/main/java/com/versilistyson/searchflix/data/remote/api/ShowApi.kt
@@ -1,6 +1,8 @@
 package com.versilistyson.searchflix.data.remote.api
 
 interface ShowApi {
+    val r: Int
+    val n: Boolean
     fun fetchShowQueryResults()
     fun fetchPopularShows()
     fun fetchTopRatedShows()
